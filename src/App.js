eee935a9e291625f1c6923207a46d3c0a1e93658
snipergn <46683784+snipergn@users.noreply.github.com>
2022-07-01 23:09:57 +0300

    two commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,13 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Navbar, Nav, NavDropdown, Button} from 'react-bootstrap';
 import { ReactComponent as Logo } from './images/logo.svg';
+import { ReactComponent as Todo } from './images/icon-todo.svg';
+import { ReactComponent as Calendar } from './images/icon-calendar.svg';
+import { ReactComponent as Reminder } from './images/icon-reminders.svg';
+import { ReactComponent as Planning } from './images/icon-planning.svg';
+
+
+
 
 const App = () => {
   return (
@@ -19,15 +26,15 @@ const App = () => {
     {/* Navbar Elements*/}
     <Nav className="me-auto">
       <NavDropdown title="Features" id="nav-dropdown">
-        <NavDropdown.Item eventKey="4.1">To Do list</NavDropdown.Item>
-        <NavDropdown.Item eventKey="4.2">Calendar</NavDropdown.Item>
-        <NavDropdown.Item eventKey="4.3">Reminder</NavDropdown.Item>
-        <NavDropdown.Item eventKey="4.3">Planning</NavDropdown.Item>
+        <NavDropdown.Item id="items" eventKey="4.1"><Todo id='icon'/>To Do list</NavDropdown.Item>
+        <NavDropdown.Item id="items" eventKey="4.2"><Calendar id='icon'/>Calendar</NavDropdown.Item>
+        <NavDropdown.Item id="items" eventKey="4.3"><Reminder id='icon'/>Reminder</NavDropdown.Item>
+        <NavDropdown.Item id="items" eventKey="4.3"><Planning id='icon'/>Planning</NavDropdown.Item>
       </NavDropdown>
       <NavDropdown title="Company" id="nav-dropdown">
-        <NavDropdown.Item eventKey="4.1">History</NavDropdown.Item>
-        <NavDropdown.Item eventKey="4.2">Our Team</NavDropdown.Item>
-        <NavDropdown.Item eventKey="4.3">Blog</NavDropdown.Item>
+        <NavDropdown.Item id="items" eventKey="4.1">History</NavDropdown.Item>
+        <NavDropdown.Item id="items" eventKey="4.2">Our Team</NavDropdown.Item>
+        <NavDropdown.Item id="items" eventKey="4.3">Blog</NavDropdown.Item>
       </NavDropdown>
       <Nav.Link href="#home">Carrers</Nav.Link>
       <Nav.Link href="#home">About</Nav.Link>
@@ -35,10 +42,10 @@ const App = () => {
 
       <Nav className="justify-content-end" activeKey="/home">
       <Nav.Item>
-        <Nav.Link href="#home" className='login1' >Login</Nav.Link>
+        <Nav.Link id='login1' href="#home">Login</Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Button variant="outline-secondary" className='button1'>Secondary</Button>
+        <Button id="button1" variant="outline-secondary">Secondary</Button>
       </Nav.Item>
       </Nav>
     </Container>
