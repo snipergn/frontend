3550a32338177f47c40ca1fa947ce9c8b7cc49c7
Sniper GN1 <andrei.banita@gmail.com>
2022-09-06 21:48:23 +0300

    commit v3

--- src/Components/Dashboard/dashboard.js
+++ src/Components/Dashboard/dashboard.js
@@ -5,15 +5,17 @@ import React, { useState, useEffect } from 'react';
 const Dashboard = () => {
 	const [userMap, setUserMap] = useState([]);
 
-	useEffect(() => {
-		fetch('https://6304d6b494b8c58fd7264985.mockapi.io/spot')
-		.then(response => response.json())
-		.then(spot => {setUserMap(userMap)})
-		.catch(err => console.log(err))
-	})
+useEffect(() => {
+    fetch('https://6304d6b494b8c58fd7264985.mockapi.io/spot')
+    .then(response => response.json())
+    .then(spot => {setUserMap(spot);
+  })
+    //  setPostArray([{name: 'a'}, {name: 'b'},{name: 'c'}])
+}, [])
+
 
 	 return (
-	  	<MapContainer center={[51.505, -0.09]} zoom={12} scrollWheelZoom={false}>
+	  	<MapContainer center={[51.505, -0.09]} zoom={4} scrollWheelZoom={false}>
 		  <TileLayer
 		    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 		    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -21,13 +23,16 @@ const Dashboard = () => {
 		  {
 		  userMap.map((spot) => {
 		  	return (
-		  		 <Marker position={[spot.lat, spot.lng]}>
-			  		 <Popup>
-				      A pretty CSS3 popup. <br /> Easily customizable.
-				      <br/>
-				      <button> Add to favorite</button>
+		  		 <Marker position={[spot.lat, spot.long]}>  		
+           		 <Tooltip><b>{spot.name}</b></Tooltip>
+           		 <Popup>
+				      	<h1 className="title">Wind Propability</h1>  
+				      	<p>LATITUDINE:</p>   
+				      	<p>LONGITUDE:</p>      
+				      	<p>WHEN TO GO:</p>
+
+				      <button className="button"> Add to favorite</button>
 				    </Popup>
-           		 <Tooltip><b>{spot.location}</b></Tooltip>
          		 </Marker>		      
 				)
 			})
