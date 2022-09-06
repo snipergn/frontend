5a251d84009e2811dbb8dfdb25e6290b0bfef295
Sniper GN1 <andrei.banita@gmail.com>
2022-09-06 19:15:46 +0300

    commit main v1

--- src/Components/Dashboard/dashboard.js
+++ src/Components/Dashboard/dashboard.js
@@ -1,18 +1,37 @@
-import { MapContainer, TileLayer, Marker, Popup} from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup, Tooltip} from 'react-leaflet';
 import './dashboard.css';
+import React, { useState, useEffect } from 'react';
 
 const Dashboard = () => {
+	const [userMap, setUserMap] = useState([]);
+
+	useEffect(() => {
+		fetch('https://6304d6b494b8c58fd7264985.mockapi.io/spot')
+		.then(response => response.json())
+		.then(spot => {setUserMap(userMap)})
+		.catch(err => console.log(err))
+	})
+
 	 return (
-	  	<MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+	  	<MapContainer center={[51.505, -0.09]} zoom={12} scrollWheelZoom={false}>
 		  <TileLayer
 		    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 		    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
 		  />
-		  <Marker position={[51.505, -0.09]}>
-		    <Popup>
-		      A pretty CSS3 popup. <br /> Easily customizable.
-		    </Popup>
-		  </Marker>
+		  {
+		  userMap.map((spot) => {
+		  	return (
+		  		 <Marker position={[spot.lat, spot.lng]}>
+			  		 <Popup>
+				      A pretty CSS3 popup. <br /> Easily customizable.
+				      <br/>
+				      <button> Add to favorite</button>
+				    </Popup>
+           		 <Tooltip><b>{spot.location}</b></Tooltip>
+         		 </Marker>		      
+				)
+			})
+		}
 		</MapContainer>
 	  );
 	}
