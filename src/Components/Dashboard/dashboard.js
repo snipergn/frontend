428e4a35c785190289cf36a1f6066b0aaa2e043f
Sniper GN1 <andrei.banita@gmail.com>
2022-09-10 01:20:42 +0300

    new commit

--- src/Components/Dashboard/dashboard.js
+++ src/Components/Dashboard/dashboard.js
@@ -9,27 +9,28 @@ const Dashboard = () => {
 	const [userMap, setUserMap] = useState([]);
 	const [isOpen, setOpen] = React.useState(false);
 
+	const itemIds = new Set(userMap.map(({ id }) => id))
 	const handleOpen = () => {
-		setOpen(true);
+		if(itemIds) {
+		setOpen(false);
+	}	
 	};
 
 	const handleClose = () => {
-		setOpen(false);
+		if(itemIds) {
+		setOpen(true);
+	}	
+
 	};
-	const FilterMap = () => {
-		userMap.forEach((spot) => {
-			if(spot.id) {
-				return true
-			} else 
-				return false
-		})
-	}
+	
 useEffect(() => {
     fetch('https://6304d6b494b8c58fd7264985.mockapi.io/spot')
     .then(response => response.json())
     .then(spot => {setUserMap(spot);
   })
 }, [])
+	
+
 
 	 return (
 	  	<MapContainer center={[51.505, -0.09]} zoom={4} scrollWheelZoom={false}>
@@ -40,22 +41,28 @@ useEffect(() => {
 		  {
 		  userMap.map((spot) => {
 		  	return (
-		  		 <Marker position={[spot.lat, spot.long]}>			  		
-           		 <Tooltip><b>{spot.name} </b></Tooltip>
-           		 <Popup className="popup">
-				      	<h1 className="title">{spot.name}</h1> 
-				      	<p className="country">{spot.country}</p>
-				      	<hr/>
-					      	<p className="para"><strong>LATITUDINE:</strong> <br/>{spot.lat}</p>   
-					      	<p className="para"><strong>LONGITUDE:</strong> <br/>{spot.long}</p>      
-					      	<p className="para"><strong>WHEN TO GO:</strong> <br/>{spot.month}</p>
-						
-						{ isOpen && FilterMap
-				 	? <div  onClick={handleClose} className="buttonclose">REMOVE FROM FAVORITE</div>				 	
-					: <div  onClick={handleOpen} className="button">ADD TO FAVORITE</div >
-				     	}
-				    </Popup>
-         		 </Marker>		      
+		  		 <div>
+		  		 
+		  		 	<Marker position={[spot.lat, spot.long]}>\
+		  		 
+           		 <Tooltip><b>{spot.name} </b></Tooltip>       	
+	           		  	 <Popup className="popup">
+					        <h1 className="title">{spot.name}</h1> 
+					     	<p className="country">{spot.country}</p>
+					      	<hr/> 
+						      	<p className="para"><strong>LATITUDINE:</strong> <br/>{spot.lat}</p>   
+						      	<p className="para"><strong>LONGITUDE:</strong> <br/>{spot.long}</p>      
+						      	<p className="para"><strong>WHEN TO GO:</strong> <br/>{spot.month}</p>
+								{ isOpen
+									?<div  onClick={handleOpen} className="button">ADD TO FAVORITE</div>
+								: <div  onClick={handleClose} className="buttonclose">REMOVE FROM FAVORITE</div>
+							}
+
+					    </Popup> 
+					    
+				    </Marker>
+				    </div>	
+				      
 				)
 			})
 		}
