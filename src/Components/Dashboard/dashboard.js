a87a28a5431e8403856c02e73f4ee49d84dbe1ef
Sniper GN1 <andrei.banita@gmail.com>
2022-09-11 16:19:30 +0300

    origin

--- src/Components/Dashboard/dashboard.js
+++ src/Components/Dashboard/dashboard.js
@@ -1,28 +1,27 @@
 import { MapContainer, TileLayer, Marker, Popup, Tooltip} from 'react-leaflet';
 import './dashboard.css';
 import React, { useState, useEffect } from 'react';
-import IconMinus from '../Assets/minus-icon.svg';
 import Star from '../Assets/star-on.png';
 
 
 const Dashboard = () => {
 	const [userMap, setUserMap] = useState([]);
-	const [isOpen, setOpen] = React.useState(false);
+	const [button, setButton] = React.useState(true);
+	const [seeButton, setSeeButton] = useState('')
 
-	const itemIds = new Set(userMap.map(({ id }) => id))
-	const handleOpen = () => {
-		if(itemIds) {
-		setOpen(false);
-	}	
-	};
 
-	const handleClose = () => {
-		if(itemIds) {
-		setOpen(true);
-	}	
+	const setButtonUp = () => {
+		setButton(false);
+		setSeeButton('REMOVE TO FAVORITE');
 
-	};
-	
+	}
+	const setButtonDown = () => {
+		setButton(true);
+		setSeeButton('ADD TO FAVORITE');
+		Object.assign(button, userMap);				
+	}
+
+
 useEffect(() => {
     fetch('https://6304d6b494b8c58fd7264985.mockapi.io/spot')
     .then(response => response.json())
@@ -30,7 +29,6 @@ useEffect(() => {
   })
 }, [])
 	
-
 
 	 return (
 	  	<MapContainer center={[51.505, -0.09]} zoom={4} scrollWheelZoom={false}>
@@ -40,29 +38,33 @@ useEffect(() => {
 		  />
 		  {
 		  userMap.map((spot) => {
-		  	return (
-		  		 <div>
-		  		 
-		  		 	<Marker position={[spot.lat, spot.long]}>\
-		  		 
+		  	return (	  		
+		  	<div> 
+		  		  <Marker key={spot.id} position={[spot.lat, spot.long]}>
            		 <Tooltip><b>{spot.name} </b></Tooltip>       	
-	           		  	 <Popup className="popup">
-					        <h1 className="title">{spot.name}</h1> 
+	           		  	 <Popup className="popup" key={spot.id}>
+					        { button
+					        ?  <h1 className="title">{spot.name}</h1>
+					        : <h1 className="title">{spot.name}
+							    <img className="icon" src={Star} width='20px' alt='Star Icon'/>
+				        	</h1>
+				        	} 
 					     	<p className="country">{spot.country}</p>
-					      	<hr/> 
+					      	<hr/>
 						      	<p className="para"><strong>LATITUDINE:</strong> <br/>{spot.lat}</p>   
 						      	<p className="para"><strong>LONGITUDE:</strong> <br/>{spot.long}</p>      
 						      	<p className="para"><strong>WHEN TO GO:</strong> <br/>{spot.month}</p>
-								{ isOpen
-									?<div  onClick={handleOpen} className="button">ADD TO FAVORITE</div>
-								: <div  onClick={handleClose} className="buttonclose">REMOVE FROM FAVORITE</div>
-							}
-
-					    </Popup> 
-					    
+						      	{ button
+						      	?	<div className="button" onClick={setButtonUp}>
+						      		{seeButton}
+						      		</div>
+						      	:	<div className="buttonclose" onClick={setButtonDown}>	
+									{seeButton}
+									</div> }
+					    </Popup>   
 				    </Marker>
-				    </div>	
-				      
+				    </div>
+   
 				)
 			})
 		}
