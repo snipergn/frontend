0727fd5344d127d627239fd5690d1031aca89014
Sniper GN1 <andrei.banita@gmail.com>
2022-09-12 21:12:35 +0300

    new version

--- src/Components/Dashboard/dashboard.js
+++ src/Components/Dashboard/dashboard.js
@@ -2,35 +2,52 @@ import { MapContainer, TileLayer, Marker, Popup, Tooltip} from 'react-leaflet';
 import './dashboard.css';
 import React, { useState, useEffect } from 'react';
 import Star from '../Assets/star-on.png';
-
+import L from 'leaflet';
 
 const Dashboard = () => {
 	const [userMap, setUserMap] = useState([]);
 	const [button, setButton] = React.useState(true);
-	const [seeButton, setSeeButton] = useState('ADD TO FAVORITE')
+	const [seeButton, setSeeButton] = useState('ADD TO FAVORITE');
 
 
 	const setButtonUp = () => {
 			setButton(false);
 			setSeeButton('REMOVE TO FAVORITE');
 		
-	}
+	};
 	const setButtonDown = () => {
 			setButton(true);
 			setSeeButton('ADD TO FAVORITE');
-			}
-
-	const SetVerify = () => {
-		userMap.forEach((spot) => {
-			if(spot.country.length > 3) {
-				return setButtonUp && setButtonUp 
-			} else {
-				return 'error';
-			}
-		})
-	}
-
-
+	};
+	
+    //  Create the Icon
+	const LeafIcon = L.Icon.extend({
+		options: {}
+	  });
+	
+	  const blueIcon = new LeafIcon({
+		  iconUrl:
+			"https://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|abcdef&chf=a,s,ee00FFFF"
+		}),
+		greenIcon = new LeafIcon({
+		  iconUrl:
+			"https://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|2ecc71&chf=a,s,ee00FFFF"
+		});
+	
+		//  Use the state hook:
+	  const [icon, setIcon] = useState(blueIcon);
+	
+		// This function will change the state's icon:
+	
+	  const changeIconColor = (icon) => {
+		if (icon.options.iconUrl === greenIcon.options.iconUrl) {
+		  setIcon((current) => (current = blueIcon));
+		} else {
+		  setIcon((current) => (current = greenIcon));
+		}
+	  };
+		
+	
 useEffect(() => {
     fetch('https://6304d6b494b8c58fd7264985.mockapi.io/spot')
     .then(response => response.json())
@@ -40,7 +57,7 @@ useEffect(() => {
 	
 
 	 return (
-	  	<MapContainer center={[51.505, -0.09]} zoom={4} scrollWheelZoom={false}>
+	  	<MapContainer center={[51.505, -0.09]}  zoom={4} scrollWheelZoom={false}>
 		  <TileLayer
 		    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 		    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -48,33 +65,45 @@ useEffect(() => {
 		  {
 		  userMap.map((spot) => {
 		  	return (	  		
-		  	<div> 
-		  		  <Marker key={spot.id} position={[spot.lat, spot.long]}>
-           		 <Tooltip><b>{spot.name} </b></Tooltip>       	
+		  		<div>
+					<Marker className="marker"
+					key={spot.id}
+					position={[spot.lat, spot.long]}
+					icon={icon}
+					>
+           		 	<Tooltip><b>{spot.name} </b></Tooltip>
 	           		  	 <Popup className="popup" key={spot.id}>
-					        { button && SetVerify
+					        { button 
 					        ?  <h1 className="title">{spot.name}</h1>
 					        : <h1 className="title">{spot.name}
 							    <img className="icon" src={Star} width='20px' alt='Star Icon'/>
-				        	</h1>
+				        	  </h1>
 				        	} 
 					     	<p className="country">{spot.country}</p>
 					      	<hr/>
 						      	<p className="para"><strong>LATITUDINE:</strong> <br/>{spot.lat}</p>   
 						      	<p className="para"><strong>LONGITUDE:</strong> <br/>{spot.long}</p>      
 						      	<p className="para"><strong>WHEN TO GO:</strong> <br/>{spot.month}</p>
-						      	{ button  && SetVerify
-						      	?	<div className="button" onClick={setButtonUp}>
+						      	{ button 
+						      	?	<div className="button" onClick={() => {
+									setButtonUp();
+									changeIconColor(icon);
+									}}>
 						      		{seeButton}
 						      		</div>
-						      	:	<div className="buttonclose" onClick={setButtonDown}>	
+						      	:	<div className="buttonclose" onClick={() => {
+									setButtonDown();
+									changeIconColor(icon);
+									}}>
 									{seeButton}
 									</div> }
-					    </Popup>   
-				    </Marker>
-				    </div>
-   
+
+								</Popup>   
+								</Marker>
+							</div>
+				
 				)
+				
 			})
 		}
 		</MapContainer>
