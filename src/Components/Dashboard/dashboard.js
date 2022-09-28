dd4bfdaa9c300f1865c6ef205c139b9fbd27dff4
Andrei <andrei.banita@gmail.com>
2022-09-28 19:33:08 +0300

    new commit

--- src/Components/Dashboard/dashboard.js
+++ src/Components/Dashboard/dashboard.js
@@ -2,30 +2,24 @@ import { MapContainer, TileLayer, Marker, Popup, Tooltip} from 'react-leaflet';
 import './dashboard.css';
 import React, { useState, useEffect } from 'react';
 import Star from '../Assets/star-on.png';
-import L, { map } from 'leaflet';
+import L from 'leaflet';
 import BasicTable from './Table';
 
 
 const Dashboard = () => {
 	const [userMap, setUserMap] = useState([]);
-	const [button, setButton] = React.useState(true);
-	const [nameButton, setnameButton] = useState('ADD TO FAVORITE')
 	const [isActive, setIsActive] = useState(false);
 	const filter = userMap.filter(spot => spot.name.length > 4)
 
 
 	const setButtonUp = () => {
-
 		if(button) {
 			setButton(false)
 			setnameButton('REMOVE FROM FAVORITE')
-			handleClick()
 		} else {
 			setButton(true);
 			setnameButton('ADD TO FAVORITE')
-			handleClick()
 		}
-	
 	};
 
 	const handleClick = event => {
@@ -59,38 +53,7 @@ const Dashboard = () => {
 		}
 	}
 
-		// A popup when is clicked the button 'Add to favorite' -> change button, star, marker color 
-		//  popup when is clicked the button 'Remove from favorite' -> Change button, star, marker color to default.
-
-		//#1 Add a button, a star icon and marker color only for Popup with id, lat, long		
-
-		const AddtoFavorite = () => {
-			<MapContainer center={[51.505, -0.09]}  zoom={4} scrollWheelZoom={false}>
-			var map = L.map('map', {
-				center: [51.505, -0.09],
-				zoom: 13
-			});
-
-			let places = 
-			<div 
-				onClick={ () => {
-				setButtonUp()}}
-				className={isActive ? 'buttonclose' : 'button'}>
-				{nameButton}
-				</div>
-			userMap.map((spot) => {
-				let popup = L.popup(places)
-             .setLatLng(spot.la, spot.long)
-             .setContent('<div>Hello world!<br />This is a nice popup.</div>')
-             .openOn(map);
-
-			})
-			</MapContainer>
-		}
-		let storageFav = AddtoFavorite()
-		//#2 When you click the button change state for button, marker and star.
-
-
+	
 
 	useEffect(() => {
 		fetch('https://6304d6b494b8c58fd7264985.mockapi.io/spot')
@@ -107,29 +70,35 @@ const Dashboard = () => {
 		attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 		url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
 		/>
-		{ filter.map(spot => {
+		{ filter.map((spot, i) => {
 		return (
 		<Marker className="marker"
 				position={[spot.lat, spot.long]}
-				key={spot.id}
+				key={i}
 				icon={icon}
-				
 				>
 			<Tooltip><b>{spot.name}</b></Tooltip>
-				<Popup className="popup" key={spot.id} >
-				{ button   
+				<Popup className="popup" key={i} >
+				{ button
 					? <h1 className="title" >{spot.name}</h1>
 					: <h1 className="title"  >{spot.name}
 						<img className="icon" src={Star} width='20px' alt='Star Icon'/>
 					</h1>
 				} 
-				<p className="country" >{spot.country}</p>
-				<hr/>
-					
+					<p className="country" >{spot.country}</p>
+					<hr/>
 					<p className="para"><strong>LATITUDINE:</strong> <br/>{spot.lat}</p>  
 					<p className="para" ><strong>LONGITUDE:</strong> <br/>{spot.long}</p>         
 					<p className="para" ><strong>WHEN TO GO:</strong> <br/>{spot.month}</p>
-					{storageFav}
+					
+						? <div onClick={() => {
+							handleClick()
+							setButtonUp()
+						}}
+						className={isActive ? 'buttonclose' : 'button'}>{nameButton}
+						</div>
+					: console.log('error')
+
 				</Popup>   
 			</Marker>
 				)
