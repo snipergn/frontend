ea05f2f45c60642b591558306b105343e98e0d94
snipergn <andrei.banita@gmail.com>
2022-10-17 19:42:42 +0300

    new commit

--- src/Components/Table/Table.js
+++ src/Components/Table/Table.js
@@ -1,66 +1,31 @@
-import React, { useState, useEffect } from 'react';
-import { makeStyles } from "@material-ui/core/styles";
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
-import SortingCountry from './SortingCountry';
+import { useState } from "react";
+import TableBody from "./TableBody";
+import TableHead from "./TableHead";
 
-	const useStyles = makeStyles({
-		table: {
-			width: 1100,
-			margin: "auto",
-			padding: 100
-		}
-	});
-	
-const BasicTable = () => {
-    const [userMap, setUserMap] = useState([]);
-	const classes = useStyles();
-	const filter = userMap.filter(spot => spot.name.length > 4)
-	const { rowData} = SortingCountry()
+const Table = () => {
+ const [tableData, setTableData] = useState([]);
 
+ const columns = [
+  { label: "name", accessor: "name_location" },
+  { label: "country", accessor: "country" },
+  { label: "latitude", accessor: "lat" },
+  { label: "longitude", accessor: "long" },
+  { label: "probability", accessor: "probability" },
+  { label: "month", accessor: "month" }
+ ];
 
-    useEffect(() => {
-		fetch('https://6304d6b494b8c58fd7264985.mockapi.io/spot')
-		.then(response => response.json())
-		.then(spot => {setUserMap(spot);
-	})
-	}, [])
+ return (
+  <>
+   <table className="table">
+    <caption>
+     Developers currently enrolled in this course, column headers are sortable.
+    </caption>
+    <TableHead columns={columns} />
+    <TableBody columns={columns} tableData={tableData} />
+   </table>
+  </>
+ );
+};
 
-    return (
-        <TableContainer component={Paper}>
-			<Table aria-label="simple table" className={classes.table}> 
-				<TableHead>
-				<TableRow>
-					<TableCell align="center">Nr.crt</TableCell>
-					<TableCell align="center">Name</TableCell>
-					<SortingCountry/>
-					<TableCell align="center">Latitude</TableCell>
-					<TableCell align="center">Longitude</TableCell>
-					<TableCell align="center">Wind probability</TableCell>
-					<TableCell align="center">When to Go</TableCell>
-				</TableRow>
-				</TableHead>
-				<TableBody rowData={rowData}>
-				{rowData.map((spot) => (
-					<TableRow key={spot.id}>
-					<TableCell align="center">{spot.id}</TableCell>
-					<TableCell align="center">{spot.name}</TableCell>
-					<TableCell align="center">{spot.country}</TableCell>
-					<TableCell align="center">{spot.lat}</TableCell>
-					<TableCell align="center">{spot.long}</TableCell>
-					<TableCell align="center">{spot.probability}</TableCell>
-					<TableCell align="center">{spot.month}</TableCell>
-					</TableRow>
-				))}
-				</TableBody>
-			</Table>
-		</TableContainer>
-    );
-  }
-  export default BasicTable
+export default Table;
   
